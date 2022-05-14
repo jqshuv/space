e30e5a321d1094923290e0e1a8638fc01dbc0fbe
jqshuv <jqshuv@gmail.com>
2022-05-14 01:30:36 +0000

    feat: :sparkles: added example file

---
+++ templates/default/index.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Unately
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+const hellotypes = [ "Hello", "Howdy", "Jello" ];
+
+function random_item(items) {
+	return items[Math.floor(Math.random()*items.length)];
+};
+
+function talk(script) {
+	console.log(random_item(script) + ' World');
+	
+};
+
+talk(hellotypes);
