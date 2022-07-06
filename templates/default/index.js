db1e9ffe4fa46526bf8eaf8436b6bed7531e5f07
Joshua Schmitt <jqshuv@gmail.com>
2022-07-07 00:55:12 +0200

    feat: :memo: add jsdoc

--- templates/default/index.js
+++ templates/default/index.js
@@ -4,13 +4,17 @@
 // https://opensource.org/licenses/MIT
 
 const hellotypes = [ "Hello", "Howdy", "Jello" ];
-
+/**
+ * @param  {array} items
+ */
 function random_item(items) {
 	return items[Math.floor(Math.random()*items.length)];
 };
-
-function talk(script) {
-	console.log(random_item(script) + ' World');
+/**
+ * @param  {string[]} hellos
+ */
+function talk(hellos) {
+	console.log(random_item(hellos) + ' World');
 	
 };
 
