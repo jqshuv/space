4d19df39c5f7410cc85d79f1d368ef9f1e8fac60
Joshua Schmitt <jqshuv@gmail.com>
2022-07-08 23:40:02 +0200

    fix: :ambulance: fix console.log

--- packages/cli/templates/default/index.js
+++ packages/cli/templates/default/index.js
@@ -4,17 +4,20 @@
 // https://opensource.org/licenses/MIT
 
 const hellotypes = ['Hello', 'Howdy', 'Jello'];
+const consola = require('consola');
+
 /**
  * @param  {array} items - Array of items to choose from a random item will be returned
  */
 function random_item(items) {
 	return items[Math.floor(Math.random() * items.length)];
 }
+
 /**
  * @param  {array} script - Array of lines to be executed
  */
 function talk(script) {
-	console.log(`${random_item(script)} World`);
+	consola.success(`${random_item(script)} World`);
 }
 
 talk(hellotypes);
